12f1941b7be57344f8cf799d2afa04a46bcde3ec
Arthur Nunes <arthurnunesc@proton.me>
2026-04-01 21:43:20 +0200

    Adjust about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,32 +6,32 @@ export default function AboutPage() {
       </h1>
       <article>
         <p>
-          I was born and raised in <a href="https://en.wikipedia.org/wiki/Fortaleza">Fortaleza</a> 🇧🇷<br></br>
-          Dropped out of college to come to <a href="https://en.wikipedia.org/wiki/Barcelona">Barcelona</a> and found work as a dev before finding a university that wanted to accept me.<br></br>
+          I was born and raised in <a href="https://en.wikipedia.org/wiki/Fortaleza">Fortaleza</a>, Brazil.<br></br>
+          Dropped out of college at 20 to come to <a href="https://en.wikipedia.org/wiki/Barcelona">Barcelona</a> and found work as a dev before finding a university that wanted to accept me.<br></br>
           I now have deep roots here and work at
-          Centric Software building web scrapers for fashion and retail sites.
+          Centric Software building web scrapers for fashion and retail sites.<br></br>
           I also studied at {" "}
-          <a href="https://42barcelona.com">42 Barcelona</a>, <a href="https://www.lavanguardia.com/vida/20230629/9071363/asi-42-campus-barcelona-unico-mundo-mejor-decision-mi-vida-brl.html" target="_blank">see if you can find me in this article</a>.
+          <a href="https://42barcelona.com">42 Barcelona</a>: <a href="https://www.lavanguardia.com/vida/20230629/9071363/asi-42-campus-barcelona-unico-mundo-mejor-decision-mi-vida-brl.html" target="_blank">see if you can find me in this article</a>(spoiler: it's hard).
         </p>
         <p>
-          I like to ride my bike. I was a Strava local legend on a segment
+          I like to ride my bike and I was a Strava local legend on a segment
           near my place for a bit and will probably be mentioning it for a while.<br></br>
           At some point I got tired of routing apps
-          having wrong surface data, so I started working on{" "}
-          <a href="https://github.com/arthurnunesc/gravel-sync">Gravel Sync</a>
+          having wrong surface data, so I started working on <b>Gravel Sync</b>
           , a mobile app for updating OpenStreetMap road conditions while
-          you&apos;re out riding. It's not out yet.
+          you&apos;re out riding. <b>It's not out yet</b>.
         </p>
         <p>
-          I run a small homelab at home. Two Fujitsu Futro S720s in a Proxmox
-          cluster, Dell Wyse as quorum. One node has HomeAssistant with
+          I also run a small homelab at home.<br></br>
+          Two Fujitsu Futro S720s in a Proxmox
+          cluster, Dell Wyse as quorum. One node has a HomeAssistant VM with
           lighting, motion, and door automations. The other has a Debian LXC with
           Docker for self-hosting and messing around. WireGuard to reach it
-          remotely. The whole thing only draws around 30W which is nice.
+          remotely.<br></br>
+          The whole thing only draws around 30W which is nice.
         </p>
         <p>
-          I spend a lot of time with AI dev tools. Cursor at work, Claude Code
-          and Gemini CLI at home, plus a bunch of others. This blog is mostly me writing about that stuff.
+          I spend a lot of time with AI dev tools. Cursor at work and pretty much everything else at home.
         </p>
         <p>
           Find me on {" "}
